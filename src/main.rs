53a1f0619b6db650fab2b71db7f0f29bb7c43ce1
gaylord-roger <49094341+gaylord-roger@users.noreply.github.com>
2023-01-24 13:54:56 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -21,13 +21,3 @@ async fn main() -> Result<(), Error> {
         sleep(Duration::from_secs(1));
     }
 }
-
-/*
-async fn send_data(dt: DateTime<Utc>) -> Result<(), Error> {
-    let socket = OscSocket::bind("127.0.0.1:0").await?;
-    socket.connect("127.0.0.1:9000").await?;
-    socket.send(("/avatar/parameters/Hour", (dt.hour() as i32,))).await?;
-    socket.send(("/avatar/parameters/Minute", (dt.minute() as i32,))).await?;
-    socket.send(("/avatar/parameters/Second", (dt.second() as i32,))).await?;
-    Ok::<(), Error>(())
-}*/
